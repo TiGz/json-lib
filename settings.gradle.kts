3865080e30ff9aec72fe18d3716e58e7ae8a6def
TiGz <ajchesney@gmail.com>
2024-06-26 20:23:02 +0100

    try to fix jitpack build

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,14 @@
 rootProject.name = "json-lib"
 include("json-core")
 
+// the version for this comes from gradle.properties and not from the toml libs file
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
 // Where can we get external 3pp dependencies from?
 dependencyResolutionManagement {
     repositories {
